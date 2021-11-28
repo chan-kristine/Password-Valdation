1544c9aeeb8515d560777f5ecf0a59ff7eea6c4b
unknown <chankristineagcay@gmail.com>
2021-11-28 08:29:11 +0800

    Finalizing the variables and conditions that are needed in the program

--- Prog2.py
+++ Prog2.py
@@ -10,19 +10,24 @@
 # Tip: loop through each character of the input then process it letter by letter
 
 #Step1 
-Password = input("Enter your password : ") #Ask for password
+Password = input("PLease enter your PASSWORD : ") #Ask for the password
 
 #Step2
 num_character = len(Password)
-special,lower, upper, digit = 0, 0, 0, 0
+special, lower, upper, digit = 0, 0, 0, 0
 
-if len(Password) >= 15):
+if (len(Password) >= 15):
     for me in Password:
-        if (me.isdidgit()):
+        if (me.isdigit()):
             digit += 1
         if (me.islower()):
             lower += 1
         if (me.isupper()):
             upper += 1
-                    
-
+        if(me == "#" or me == "!" or me == "$" or me == "&" or me == "%" or me == "'" or me == "*" or me == "()" or me == "+ " or me == "." \
+        or me == """|""" or me == """^""" or me == "{" or me == "}" or me == "_" or me == "~" or me == """+""" ):
+            special =+ 1
+    if (lower >= 1 and upper >= 1 and special >= 1 and digit >= 1):
+                print("Your PASSWORD is VALID!")  
+    else:
+        print("Sorry! The PASSWORD you have entered is INVALID ❌")
