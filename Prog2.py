a349b4f17a6ad3e72e0e83ccf9ce3ae725adbb18
unknown <chankristineagcay@gmail.com>
2021-11-28 23:27:16 +0800

    Final Revision: renaming some variables , adding comments and ansi codes for better understanding and apperance of the program.

--- Prog2.py
+++ Prog2.py
@@ -10,24 +10,55 @@
 # Tip: loop through each character of the input then process it letter by letter
 
 #Step1 
-Password = input("Please enter your \33[32m\33[1mPASSWORD\33[0m : ") #Ask for the password
+VPassword = input("Please enter your \33[32m\33[1mPASSWORD\33[0m : ") #Ask for the password
 
 #Step2
-num_character = len(Password)
-special, lower, upper, digit = 0, 0, 0, 0
+num_character = len(VPassword)
+specialC, lower, upper, digit = 0, 0, 0, 0
 
-if (len(Password) >= 15):
-    for me in Password:
+if (len(VPassword) >= 15):
+    for me in VPassword:
         if (me.isdigit()):
             digit += 1
         if (me.islower()):
             lower += 1
         if (me.isupper()):
             upper += 1
-        if(me == "#" or me == "!" or me == "$" or me == "&" or me == "%" or me == "'" or me == "*" or me == "()" or me == "+ " or me == "." \
-        or me == """|""" or me == """^""" or me == "{" or me == "}" or me == "_" or me == "~" or me == """+""" ):
-            special =+ 1
-    if (lower >= 1 and upper >= 1 and special >= 1 and digit >= 1):
+        if (me == "#" or me == "@" or me == "=" or me == "!" or me == "$" or me == "&" or me == "%" or me == "'" or me == "*" or me == "()" or me == "+ " or me == "." \
+        or me == """|""" or me == """^""" or me == "{" or me == "}" or me == "[" or me == "]" or me == "_" or me == "~" or me == "," or me == "?" or me == """+"""):
+            specialC += 1
+
+    if (lower >= 1 and upper >= 1 and specialC >= 1 and digit >= 1):
                 print("Your \33[32m\33[1mPASSWORD\33[0m is \33[1m\33[95mVALID\33[0m!")  
-    else:
+    if (digit < 1 ):
+        print ("Oh no ! Your password must have \33[96m\33[1m\33[3matleast one number.\33[0m")
+    if (upper < 1):
+        print ("Oh no! Your password must have \33[96m\33[1m\33[3matleast one capital letter.\33[0m")    
+    if (specialC < 1):
+        print("Oh no! Your password must have \33[96m\33[1m\33[3matleast one special character.\33[0m")
+    if (lower <= 1 and upper <= 1 and specialC <=1 and digit <= 1):
         print("Sorry! The \33[32m\33[1mPASSWORD\33[0m you have entered is \33[31m\33[1mINVALID\33[0m❌")
+else:
+    for me in VPassword:
+        if (me.isdigit()):
+            digit += 1
+        if (me.islower()):
+            lower += 1
+        if (me.isupper()):
+            upper += 1
+        if (me == "#" or me == "@" or me == "=" or me == "!" or me == "$" or me == "&" or me == "%" or me == "'" or me == "*" or me == "()" or me == "+ " or me == "." \
+        or me == """|""" or me == """^""" or me == "{" or me == "}" or me == "[" or me == "]" or me == "_" or me == "~" or me == "," or me == "?" or me == """+""" ):
+            specialC += 1
+
+    print ("Oh no! Your password must have \33[96m\33[1m\33[3matleast fifteen characters.\33[0m")
+    if (digit < 1):
+        print ("Oh no! Your password must have \33[96m\33[1m\33[3matleast one number.\33[0m")
+    if (upper < 1):
+        print ("Oh no! Your password must have \33[96m\33[1m\33[3matleast one capital letter.\33[0m")
+    if (specialC < 1):
+        print("Oh no! Your password must have \33[96m\33[1m\33[3matleast one special character.\33[0m")
+
+    print("Sorry! The \33[32m\33[1mPASSWORD\33[0m you have entered is \33[31m\33[1mINVALID\33[0m❌")
+
+
+        
